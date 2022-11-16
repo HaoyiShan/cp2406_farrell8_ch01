6a7a6730a7ca03c0e407387e492f210170ba4f16
HaoyiShan <110078010+HaoyiShan@users.noreply.github.com>
2022-11-16 16:05:07 +0800

    second commit

---
+++ Task/MovieQuote.java
@@ -0,0 +1,5 @@
+public class MovieQuote {
+    public static void main(String[] args){
+        System.out.println("I guess it comes down a simple choice. Get busy living or get busy dying." );
+    }
+}
---
+++ Task/MovieQuoteInfo.java
@@ -0,0 +1,7 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args){
+        System.out.println("<The Shawshank Redemption>-1994:");
+        System.out.print("'I guess it comes down a simple choice. Get busy living or get busy dying.'");
+        System.out.println("___Andy Dufresne");
+    }
+}
